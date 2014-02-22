6f18f03f6e86bdc7b7c094316b618d91524b1be3
Dave <jy02706485@163.com>
2014-02-22 23:45:08 +0800

    2222222222222222222增加record界面

--- Com.dem.on/src/com/dem/on/record.java
+++ Com.dem.on/src/com/dem/on/record.java
@@ -34,10 +34,13 @@ import android.os.PowerManager.WakeLock;
 import android.provider.MediaStore;
 import android.widget.AdapterView;
 
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
@@ -48,6 +51,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
 import android.view.View.OnCreateContextMenuListener;
@@ -59,6 +63,9 @@ import com.dem.on.RecordHelper;
 import com.dem.on.MySQLiteOpenHelper;
 import com.dem.on.RecordButton;
 import com.dem.on.RecordButton.OnFinishedRecordListener;
+import com.dem.on.setup.MyAdapter;
+import com.dem.on.setup.ViewHolder;
+import com.dem.on.setup.ViewHolderHead;
 import com.dem.on.config;
 
 import android.database.Cursor;
@@ -94,15 +101,48 @@ RecordHelper.OnStateChangedListener, OnCompletionListener, OnErrorListener{
 	
 	private String strSampleFilePath;
 	
+	private ListView list = null;
+	private ArrayList<HashMap<String, Object>> listItem = new ArrayList<HashMap<String, Object>>();
+	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.record_activity);
+        setContentView(R.layout.recordhome_activity);
         imageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),"image.jpg"));
         sql = new MySQLiteWorker(this);
         sql.CreateDataTable(MySQLiteOpenHelper.MYRECORD_TABLE_NAME);
         sql.EnumDataBase(MySQLiteOpenHelper.MYRECORD_TABLE_NAME);
-        setupViews();
+        //setupViews();
+        InitListView();
     }
+	
+	private void InitListView(){
+		list = (ListView) findViewById(R.id.ListViewUserData);
+		list.setVerticalScrollBarEnabled(false);
+		
+		HashMap<String, Object> map = new HashMap<String, Object>();  
+        map.put("ItemBigPicture_head", R.drawable.wenzhang);
+        listItem.add(map);
+        
+        
+        HashMap<String, Object> map1 = new HashMap<String, Object>();  
+        map1.put("Itemtouxiang", R.drawable.gaoyuanyuan);
+        map1.put("huangguan", R.drawable.wenzhang);
+        map1.put("ItemBigPicture", R.drawable.gaoyuanyuan);
+        map1.put("Itemusername", "谢娜");
+        map1.put("Itemchenghao", "叫醒宗师");
+        map1.put("Itemshijian", "2/22 13:50");
+        
+        map1.put("Itemleijijiaoxing_shu", "56312728");
+        map1.put("Itemjiaoxinglv_shu", "91%");
+        map1.put("ItemButtonYuyin", "语音");
+        map1.put("ItemButtonDaoju", "道具");
+       
+        listItem.add(map1);
+
+        MyAdapter listItemAdapter = new MyAdapter(this);
+
+        list.setAdapter(listItemAdapter);  
+	}
 	
 	public void startPlayback(String strfilname)
 	{
@@ -471,4 +511,126 @@ RecordHelper.OnStateChangedListener, OnCompletionListener, OnErrorListener{
     	 }
     	 return bitmap;
     	}
+    
+    public class MyAdapter extends BaseAdapter {  
+		  
+        private LayoutInflater mInflater;  
+  
+        public MyAdapter(Context context) {  
+            this.mInflater = LayoutInflater.from(context);  
+        }  
+  
+        @Override  
+        public int getCount() {  
+            // TODO Auto-generated method stub  
+            return listItem.size();  
+        }  
+  
+        @Override  
+        public Object getItem(int position) {  
+            // TODO Auto-generated method stub  
+            return null;  
+        }  
+  
+        @Override  
+        public long getItemId(int position) {  
+            // TODO Auto-generated method stub  
+            return 0;  
+        }  
+        //****************************************final方法  
+        //注意原本getView方法中的int position变量是非final的，现在改为final  
+        @Override  
+        public View getView(final int position, View convertView, ViewGroup parent) {  
+             
+        	if(position == 0){
+        		ViewHolderHead holder = null;  
+                if (convertView == null) { 
+                    holder = new ViewHolderHead();    
+                    //可以理解为从vlist获取view  之后把view返回给ListView  
+                    convertView = mInflater.inflate(R.layout.userdatahead, null);  
+                    
+                    holder.ItemBigPicture_head = (ImageView)convertView.findViewById(R.id.ItemBigPicture_head);  
+                    holder.ItemButtonPlayRecord_head = (Button)convertView.findViewById(R.id.ItemButtonPlayRecord_head);  
+                    holder.ItemButtonLike_head = (Button)convertView.findViewById(R.id.ItemButtonLike_head);  
+                    holder.ItemButtonListen_head = (Button)convertView.findViewById(R.id.ItemButtonListen_head);  
+                    holder.ItemButtonTalk_head = (Button)convertView.findViewById(R.id.ItemButtonTalk_head); 
+                    
+                    convertView.setTag(holder);               
+                }else {               
+                    holder = (ViewHolderHead)convertView.getTag();  
+                }         
+                  
+                holder.ItemBigPicture_head.setBackgroundResource((Integer)listItem.get(position).get("ItemBigPicture_head"));  
+                
+                return convertView;
+        	}else{
+        		
+        		ViewHolder holder = null;  
+                if (convertView == null) { 
+                    holder = new ViewHolder();    
+                    //可以理解为从vlist获取view  之后把view返回给ListView  
+                    convertView = mInflater.inflate(R.layout.userdatalistitem, null);  
+                    
+                    holder.Itemtouxiang = (ImageButton)convertView.findViewById(R.id.Itemtouxiang);  
+                    holder.huangguan = (ImageButton)convertView.findViewById(R.id.huangguan);  
+                    holder.ItemBigPicture = (ImageView)convertView.findViewById(R.id.ItemBigPicture);  
+                    
+                    holder.Itemusername = (TextView)convertView.findViewById(R.id.Itemusername);  
+                    holder.Itemchenghao = (TextView)convertView.findViewById(R.id.Itemchenghao); 
+                    holder.Itemshijian = (TextView)convertView.findViewById(R.id.Itemshijian); 
+                    
+                    
+                    holder.Itemleijijiaoxing_shu = (TextView)convertView.findViewById(R.id.Itemleijijiaoxing_shu);
+                    holder.Itemjiaoxinglv_shu = (TextView)convertView.findViewById(R.id.Itemjiaoxinglv_shu);
+                    holder.ItemButtonYuyin = (Button)convertView.findViewById(R.id.ItemButtonYuyin);
+                    holder.ItemButtonDaoju = (Button)convertView.findViewById(R.id.ItemButtonDaoju);
+                   
+                    
+                    convertView.setTag(holder);               
+                }else {               
+                    holder = (ViewHolder)convertView.getTag();  
+                }         
+                
+               /**/ holder.Itemtouxiang.setBackgroundResource((Integer)listItem.get(position).get("Itemtouxiang"));
+                holder.huangguan.setBackgroundResource((Integer)listItem.get(position).get("huangguan"));
+                holder.ItemBigPicture.setBackgroundResource((Integer)listItem.get(position).get("ItemBigPicture"));
+                
+                holder.Itemusername.setText((String)listItem.get(position).get("Itemusername"));
+                holder.Itemchenghao.setText((String)listItem.get(position).get("Itemchenghao"));
+                holder.Itemshijian.setText((String)listItem.get(position).get("Itemshijian"));
+
+                holder.Itemleijijiaoxing_shu.setText((String)listItem.get(position).get("Itemleijijiaoxing_shu"));
+                holder.Itemjiaoxinglv_shu.setText((String)listItem.get(position).get("Itemjiaoxinglv_shu"));
+                holder.ItemButtonYuyin.setText((String)listItem.get(position).get("ItemButtonYuyin"));
+                holder.ItemButtonDaoju.setText((String)listItem.get(position).get("ItemButtonDaoju"));
+                
+                return convertView;
+        	}
+        }  
+    }  
+	
+	public final class ViewHolder {   
+        public ImageButton Itemtouxiang;  
+        public ImageButton huangguan;  
+        public ImageView ItemBigPicture;
+        
+        public TextView Itemusername;  
+        public TextView Itemchenghao; 
+        public TextView Itemshijian;
+        
+        public TextView Itemleijijiaoxing_shu;
+        public TextView Itemjiaoxinglv_shu;
+        public Button ItemButtonYuyin;
+        public Button ItemButtonDaoju;
+        
+    }
+	
+	
+	public final class ViewHolderHead {   
+        public ImageView ItemBigPicture_head;  
+        public Button ItemButtonPlayRecord_head;  
+        public Button ItemButtonLike_head;  
+        public Button ItemButtonListen_head; 
+        public Button ItemButtonTalk_head;
+    }
 }
